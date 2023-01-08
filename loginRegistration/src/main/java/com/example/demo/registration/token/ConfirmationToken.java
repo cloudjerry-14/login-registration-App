779b789fa82c60383bdb69077326ea478c4163cb
csjerry <ashutoshparihar0@gmail.com>
2023-01-08 21:10:20 +0530

    project

---
+++ loginRegistration/src/main/java/com/example/demo/registration/token/ConfirmationToken.java
@@ -0,0 +1,120 @@
+package com.example.demo.registration.token;
+
+import com.example.demo.appuser.AppUser;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class ConfirmationToken {
+
+    public ConfirmationToken() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ConfirmationToken(Long id, String token, LocalDateTime createdAt, LocalDateTime expiresAt,
+			LocalDateTime confirmedAt, AppUser appUser) {
+		super();
+		this.id = id;
+		this.token = token;
+		this.createdAt = createdAt;
+		this.expiresAt = expiresAt;
+		this.confirmedAt = confirmedAt;
+		this.appUser = appUser;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public LocalDateTime getExpiresAt() {
+		return expiresAt;
+	}
+
+	public void setExpiresAt(LocalDateTime expiresAt) {
+		this.expiresAt = expiresAt;
+	}
+
+	public LocalDateTime getConfirmedAt() {
+		return confirmedAt;
+	}
+
+	public void setConfirmedAt(LocalDateTime confirmedAt) {
+		this.confirmedAt = confirmedAt;
+	}
+
+	public AppUser getAppUser() {
+		return appUser;
+	}
+
+	public void setAppUser(AppUser appUser) {
+		this.appUser = appUser;
+	}
+
+	@SequenceGenerator(
+            name = "confirmation_token_sequence",
+            sequenceName = "confirmation_token_sequence",
+            allocationSize = 1
+    )
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "confirmation_token_sequence"
+    )
+    private Long id;
+
+    @Column(nullable = false)
+    private String token;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime expiresAt;
+
+    private LocalDateTime confirmedAt;
+
+    @ManyToOne
+    @JoinColumn(
+            nullable = false,
+            name = "app_user_id"
+    )
+    private AppUser appUser;
+
+    public ConfirmationToken(String token,
+                             LocalDateTime createdAt,
+                             LocalDateTime expiresAt,
+                             AppUser appUser) {
+        this.token = token;
+        this.createdAt = createdAt;
+        this.expiresAt = expiresAt;
+        this.appUser = appUser;
+    }
+}
